b942aa8a2606d83bc6add640c8a8d04b38f278ec
Johanne Blikberg Herheim <joher2128@uib.no>
2026-05-13 19:24:26 +0200

    Add landing page pic

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
+import Image from "next/image";
+import picture from "@/components/images/framside.png";
+
 export default function Home() {
   return (
-    <div className="bg-blue-100 w-full text-center min-h-screen items-center">
-      <h1>VOSSAGRAVEL</h1>
-      <p>7. juni</p>
+    <div className="relative flex items-center justify-center h-[calc(100vh-64px)] overflow-hidden">
+      <Image
+        src={picture}
+        alt="Vossagravel landscape"
+        fill
+        className="object-cover object-center"
+        priority
+      />
+
+      <div className="absolute inset-0 bg-black/40" />
+
+      <div className="absolute top-14 z-10 w-full sm:w-fit rounded text-white">
+        <h1 className="text-center mb-4 font-bold">Vossagravel</h1>
+        <h4 className="text-center mb-8">Norway Gravel Series 2026</h4>
+        <h1 className="font-extrabold tracking-wide text-2xl sm:text-5xl text-center px-8">
+          07.06.26
+        </h1>
+      </div>
     </div>
   );
 }
