f66160490ac071ce48ebc9b7cfa328dc8718d3af
Johanne Blikberg Herheim <joher2128@uib.no>
2026-05-21 13:03:57 +0200

    Add youtube button

--- app/page.tsx
+++ app/page.tsx
@@ -19,13 +19,21 @@ export default function Home() {
         <h1 className="italic font-bold tracking-wide mb-4">VOSSAGRAVEL</h1>
         <h3 className="italic font-semibold tracking-wide">07.06.26</h3>
         <h4 className="italic font-semibold">Norway Gravel Series 2026</h4>
-        <div className="my-10">
+        <div className="flex gap-4 my-10 items-center justify-center">
           <Link
             href="https://live.eqtiming.com/82974#dashboard"
             target="_blank"
-            className="bg-green-800 border-green-700 border-2 hover:bg-green-700 py-2 px-4 rounded-lg italic hover:animate-none animate-pulse tracking-wide font-semibold text-lg"
+            className="w-40 text-center bg-green-800/90 border-green-700 border-2 hover:bg-green-700 py-2 px-4 rounded-lg italic tracking-wide font-semibold text-lg"
           >
             Påmelding
+          </Link>
+
+          <Link
+            href="https://www.youtube.com/watch?v=Dfy7GgMYa2o&t=13s"
+            target="_blank"
+            className="w-40 text-center bg-red-800/90 border-red-700 border-2 hover:bg-red-700 py-2 px-4 rounded-lg italic tracking-wide font-semibold text-lg"
+          >
+            Youtube
           </Link>
         </div>
       </div>
